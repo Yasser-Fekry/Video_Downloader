df804648ed037e1d523bb0ecc00fd471a9bff535
R E M E N D O (IT) <fekryyasser220@gmail.com>
2024-12-16 23:58:52 +0200

    add Color

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -7,14 +7,17 @@ root = Tk()
 root.geometry('500x300')
 root.resizable(0,0)
 root.title(" Youtube Video Download  ")
+root.configure(bg = "aqua")
 
-Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
+
+Label(root,text = 'Youtube Video Downloader 💻☕', font ='arial 20 bold' , bg="aqua").pack()
 
 ##enter link
 link = StringVar()
 
-Label(root, text = 'Paste Link Here:', font = 'arial 15 bold').place(x= 160 , y = 60)
-link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 90)
+Label(root, text = 'Paste Link Here  Brooo 🔥😎 ', font = 'arial 18 bold', bg = "aqua").place(x= 100 , y = 60)
+link_enter = Entry(root, width = 70,textvariable = link).place(x = 40, y =  100)
+
 
 #function to download video
 def download_video():
@@ -23,14 +26,15 @@ def download_video():
     video.download()
     Label(root, text = 'Video_Download', font = 'arial 15').place(x= 150 , y = 210)
 
+
 def download_audio():
     url = YouTube(str(link.get()))
     audio = url.streams.filter(only_audio=True).first()
     audio.download()
     Label(root, text = 'Audio_Download', font = 'arial 15').place(x= 150 , y = 250)
 
-Button(root,text = 'Video Download', font = 'arial 12 bold' ,bg = 'pale violet red', padx = 2, command = download_video).place(x=100 ,y = 150)
-Button(root,text = 'Audio Download', font = 'arial 12 bold' ,bg = 'light blue', padx = 2, command = download_audio).place(x=300 ,y = 150)
+Button(root,text = 'Video Download', font = 'arial 12 bold' ,bg = 'pale violet red', padx = 2, activebackground= "green",command = download_video).place(x=100 ,y = 150)
+Button(root,text = 'Audio Download', font = 'arial 12 bold' ,bg = 'light blue', padx = 2,activebackground="green" , command = download_audio).place(x=300 ,y = 150)
 
 root.mainloop()
 
